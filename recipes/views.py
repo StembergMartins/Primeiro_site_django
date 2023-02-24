1cd996b7ec5d820928b74864a3beb13ace8151b3
STEMBERG MARTINS DA SILVA CARVALHO <stemberg.martins@gmail.com>
2023-02-24 16:30:25 -0300

    Include_header.html

--- recipes/views.py
+++ recipes/views.py
@@ -3,18 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'recipes/home.html', context={
+    return render(request,'recipes/pages/home.html', context={
         'name' : 'Stemberg Carvalho'
     })
 
-
-def sobre(request):
-    return render(request, 'global/home.html')
-
-
-def contato(request):
-    return render(request,'recipes/contato.html')
-
-
-def contato(request):
-    return render(request,'recipes/index.html')
